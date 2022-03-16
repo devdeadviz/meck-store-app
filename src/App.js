80d2a1bfcb55fc8f0133a2d37423357d3137315f
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-03-16 20:49:01 +0530

    feat: add navbar and cl styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Mockman from "mockman-js";
+import { Homepage } from "./pages";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Homepage />} />
         <Route path="/mock" element={<Mockman />} />
       </Routes>
     </div>

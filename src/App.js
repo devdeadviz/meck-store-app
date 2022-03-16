bfe43eef2d0f122434fc9952cd8a2147d71e7a6c
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-03-16 19:54:04 +0530

    feat: add router and mockman route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Mockman from "mockman-js";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/mock" element={<Mockman />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

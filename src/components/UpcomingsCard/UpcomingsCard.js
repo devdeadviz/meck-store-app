b93ea85546fac6cc924737dcac4948b12d4843ca
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-03-21 02:33:36 +0530

    feat: add upcoming cards

---
+++ src/components/UpcomingsCard/UpcomingsCard.js
@@ -0,0 +1,40 @@
+const UpcomingsCard = ({ title, subTitle, price, image }) => {
+  return (
+    <div class="horizontal-card-wrapper flex m-5">
+      <div class="horizontal-card-image-wrapper flex flexAlignItemsCenter">
+        <img class="responsive-image px-2" src={image} alt={title} />
+      </div>
+      <div class="card-body-wrapper flex flexCol">
+        <div class="horizontal-card-header">
+          <h2 class="my-3 mx-4">{title}</h2>
+          <small class="my-2 mx-4">{subTitle}</small>
+          <div class="simple-rating-wrapper flex mx-4">
+            <div class="mt-3 mr-2">
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+            </div>
+          </div>
+        </div>
+        <div class="horizontal-card-price-wrapper flex flexCol mt-4 mb-2 mx-4">
+          <div>
+            <span class="product-discount-price">{price}</span>
+          </div>
+        </div>
+        <div class="horizontal-card-footer mx-5 mb-5">
+          <button
+            type="button"
+            class="btn btn-outline-primary wishlist-btn mt-4"
+          >
+            <i class="fas fa-bell mr-2"></i>
+            Get Notified
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { UpcomingsCard };
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { Navbar } from "./Navbar/Navbar";
 export { CategoriesCard } from "./CategoriesCard/CategoriesCard";
+export { UpcomingsCard } from "./UpcomingsCard/UpcomingsCard";

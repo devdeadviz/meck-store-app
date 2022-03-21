14bfa8976fc1b6970f766b99c95ac3fb3ac918a4
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-03-21 17:10:39 +0530

    feat: add products

---
+++ src/components/ProductsCard/ProductsCard.js
@@ -0,0 +1,29 @@
+import "./ProductsCard.css";
+
+const ProductsCard = ({ title, image, price }) => {
+  return (
+    <div class="vertical-card-wrapper">
+      <div class="vertical-card-image-wrapper">
+        <img class="responsive-image" src={image} alt={title} />
+      </div>
+      <div class="vertical-card-header">
+        <h2 class="my-3 mx-3">{title}</h2>
+      </div>
+      <div class="vertical-card-body my-4 mx-2">
+        <div class="vertical-card-price-wrapper mt-2">
+          <div class="mb-4 mx-2">
+            <span class="product-discount-price">{price}</span>
+          </div>
+        </div>
+      </div>
+      <div class="vertical-card-footer">
+        <button type="button" class="btn btn-primary add-btn my-4 mx-2">
+          <i class="fas fa-shopping-cart mr-2"></i>
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export { ProductsCard };
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { CategoriesCard } from "./CategoriesCard/CategoriesCard";
 export { UpcomingsCard } from "./UpcomingsCard/UpcomingsCard";
 export { Footer } from "./Footer/Footer";
 export { Filter } from "./Filter/Filter";
+export { ProductsCard } from "./ProductsCard/ProductsCard";

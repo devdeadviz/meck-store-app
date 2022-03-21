a4ff3b49f47fd19bfe892edbdea3c9ba1e593c65
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-03-21 22:38:22 +0530

    fix: update class to className

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,8 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer class="store-footer text-center">
-      <p class="py-4">
+    <footer className="store-footer text-center">
+      <p className="py-4">
         Build with ❤️ by <b>Kuldeep Gupta</b>
       </p>
     </footer>
--- src/pages/Home/Homepage.js
+++ src/pages/Home/Homepage.js
@@ -29,33 +29,33 @@ const Homepage = () => {
   return (
     <div>
       <Navbar />
-      <div class="store-image-wrapper">
+      <div className="store-image-wrapper">
         <img
-          class="store-image"
+          className="store-image"
           src="https://images.unsplash.com/photo-1595225476474-87563907a212?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
           alt="homepage"
         />
       </div>
-      <div class="store-image-content flex flexCol">
+      <div className="store-image-content flex flexCol">
         <h1>Meck Store</h1>
         <a href="#store-categories">
-          <button type="button" class="btn btn-primary m-2">
+          <button type="button" className="btn btn-primary m-2">
             Explore More
           </button>
         </a>
       </div>
-      <h1 class="store-heading text-center" id="store-categories">
+      <h1 className="store-heading text-center" id="store-categories">
         CATEGORIES
       </h1>
-      <div class="store-cards-wrapper flex flexWrap flexJustifyCenter m-5">
+      <div className="store-cards-wrapper flex flexWrap flexJustifyCenter m-5">
         {productCategories.map(({ categoryName, _id, image }) => (
           <Link className="link" to="/products" key={_id}>
             <CategoriesCard categoryName={categoryName} image={image} />
           </Link>
         ))}
       </div>
-      <h1 class="store-heading text-center">UPCOMINGS</h1>
-      <div class="store-cards-wrapper flex flexWrap flexJustifyAround m-5">
+      <h1 className="store-heading text-center">UPCOMINGS</h1>
+      <div className="store-cards-wrapper flex flexWrap flexJustifyAround m-5">
         {upcomingProducts.map(
           ({ _id, title, subTitle, price, image, upcoming }) =>
             upcoming && (

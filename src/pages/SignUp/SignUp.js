5b44ab53f2659463cb0d95c2c5ed03ea862414a9
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-03-24 23:44:19 +0530

    feat: add more inputs field in signup form

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { Footer, Navbar } from "../../components";
 import "./SignUp.css";
 
+// email, first name, last name, password & confirm password.
+
 const SignUp = () => {
   return (
     <>
@@ -12,42 +14,67 @@ const SignUp = () => {
             <h2 class="m-4 text-center">SignUp</h2>
           </div>
           <div class="vertical-card-body my-4 mx-3">
-            <form action="#">
-              <label for="email-input" class="my-3 py-5">
-                Email address
-              </label>
-              <input
-                type="email"
-                id="email-input"
-                name="email"
-                placeholder="kuldeep@gmail.com"
-              />
-              <label for="password-input" class="my-3">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password-input"
-                name="name"
-                placeholder="*********"
-              />
-              <div class="form-options flex flexJustifyBetween flexAlignItemsCenter mt-3 mb-5">
-                <label for="remember">
-                  <input
-                    class="mr-2"
-                    type="checkbox"
-                    id="remember"
-                    name="rememberme"
-                    value="RememberMe"
-                  />
-                  I accept all Terms & Conditions
+            <form id="signup-form" action="#">
+              <fieldset form="signup-form">
+                <label for="first-name-input">First Name</label>
+                <input
+                  id="first-name-input"
+                  type="text"
+                  name="firstname"
+                  placeholder="Kuldeep"
+                />
+                <label for="last-name-input">Last Name</label>
+                <input
+                  id="last-name-input"
+                  type="text"
+                  name="lastname"
+                  placeholder="Gupta"
+                />
+                <label for="email-input" class="my-3 py-5">
+                  Email address
                 </label>
-              </div>
-              <input
-                type="submit"
-                class="btn btn-primary submit-btn"
-                value="Create New Account"
-              />
+                <input
+                  type="email"
+                  id="email-input"
+                  name="email"
+                  placeholder="kuldeep@gmail.com"
+                />
+                <label for="password-input" class="my-3">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password-input"
+                  name="password"
+                  placeholder="*********"
+                />
+                <label for="-confirm-password-input" class="my-3">
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  id="confirm-password-input"
+                  name="confirm-password"
+                  placeholder="*********"
+                />
+                <div class="form-options flex flexJustifyBetween flexAlignItemsCenter mt-3 mb-5">
+                  <label for="remember">
+                    <input
+                      class="mr-2"
+                      type="checkbox"
+                      id="remember"
+                      name="rememberme"
+                      value="RememberMe"
+                    />
+                    I accept all Terms & Conditions
+                  </label>
+                </div>
+                <input
+                  type="submit"
+                  class="btn btn-primary submit-btn"
+                  value="Create New Account"
+                />
+              </fieldset>
             </form>
           </div>
           <div class="vertical-card-footer text-center my-5">

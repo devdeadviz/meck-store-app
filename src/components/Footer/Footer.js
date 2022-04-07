5c33671fba108f54b7deccc6e3395be28276dc0c
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-04-07 19:28:34 +0530

    fix: update class to className

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,17 +7,33 @@ const Footer = () => {
         Build with ❤️ by <b>Kuldeep Gupta</b>
       </p>
       <section>
-        <a href="https://github.com/devdeadviz" target="_blank" rel="noreferrer">
-          <i class="fa-brands fa-github fa-2x mx-5 mb-4"></i>
+        <a
+          href="https://github.com/devdeadviz"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fa-brands fa-github fa-2x mx-5 mb-4"></i>
         </a>
-        <a href="https://twitter.com/devdeadviz" target="_blank" rel="noreferrer">
-          <i class="fa-brands fa-twitter fa-2x mx-5 mb-4"></i>
+        <a
+          href="https://twitter.com/devdeadviz"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fa-brands fa-twitter fa-2x mx-5 mb-4"></i>
         </a>
-        <a href="https://www.linkedin.com/in/devdeadviz/" target="_blank" rel="noreferrer">
-          <i class="fa-brands fa-linkedin fa-2x mx-5 mb-4"></i>
+        <a
+          href="https://www.linkedin.com/in/devdeadviz/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fa-brands fa-linkedin fa-2x mx-5 mb-4"></i>
         </a>
-        <a href="https://www.instagram.com/devdeadviz/" target="_blank" rel="noreferrer">
-          <i class="fa-brands fa-instagram fa-2x mx-5 mb-4"></i>
+        <a
+          href="https://www.instagram.com/devdeadviz/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fa-brands fa-instagram fa-2x mx-5 mb-4"></i>
         </a>
       </section>
     </footer>

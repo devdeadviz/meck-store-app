0ca43fa639d54c3b774e442c460dcb0b69936f87
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-04-07 19:11:08 +0530

    feat: add social media icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,20 @@ const Footer = () => {
       <p className="py-4">
         Build with ❤️ by <b>Kuldeep Gupta</b>
       </p>
+      <section>
+        <a href="https://github.com/devdeadviz" target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-github fa-2x mx-5 mb-4"></i>
+        </a>
+        <a href="https://twitter.com/devdeadviz" target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-twitter fa-2x mx-5 mb-4"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/devdeadviz/" target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-linkedin fa-2x mx-5 mb-4"></i>
+        </a>
+        <a href="https://www.instagram.com/devdeadviz/" target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-instagram fa-2x mx-5 mb-4"></i>
+        </a>
+      </section>
     </footer>
   );
 };
